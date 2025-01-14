130285bdb6c04958ff7a30aa6604e68162af803d
Divyansh-Chaubey <chaubeydivyansh91@gmail.com>
2025-01-14 17:45:17 +0530

    update

---
+++ javascript/method.js
@@ -0,0 +1,76 @@
+
+// ==============================charAt()=======================================
+
+// let str = "Hello, world!";
+
+
+// let firstChar = str.charAt(0);
+
+
+// let eighthChar = str.charAt(7);
+
+
+// console.log(`The first character is: ${firstChar}`);
+// console.log(`The eighth character is: ${eighthChar}`);
+
+
+// =====================================================charCodeAt()============================================================
+
+
+// let str = "Hello, world!";
+
+
+// let firstCharCode = str.charCodeAt(0);
+
+
+// let eighthCharCode = str.charCodeAt(7);
+
+
+// console.log(`The Unicode of the first character is: ${firstCharCode}`);
+// console.log(`The Unicode of the eighth character is: ${eighthCharCode}`);
+
+
+
+
+// =================================================split()============================================================
+
+// let str = "Hello, world! How are you today?";
+
+
+// let words = str.split(" ");
+
+
+// console.log(words);
+
+
+
+
+// ===================================================================indexof()============================================================
+
+
+// let str = "Hello, world! How are you today?";
+
+
+// let index = str.indexOf("world");
+
+
+// console.log(`The index of "world" is: ${index}`);
+
+
+// let nonExistentIndex = str.indexOf("moon");
+
+
+// console.log(`The index of "moon" is: ${nonExistentIndex}`);
+
+
+
+
+
+
+// replace()============================================================
+
+
+// replace(/searchValue/gi, "newValue")************************************************
+
+// g    for globalThi
+// gi   for global and case-insensitive
